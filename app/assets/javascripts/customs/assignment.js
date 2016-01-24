cdced56eda456f563a30531c07deec5f7177435a
TrungNT <hellovietnam93@gmail.com>
2016-01-24 16:33:06 +0700

    Edit modal

--- app/assets/javascripts/customs/assignment.js
+++ app/assets/javascripts/customs/assignment.js
@@ -16,29 +16,55 @@ $(document).on("ready", function() {
     $(this).find(".assignment-start-date").html(assignmentStartDate);
     $(this).find(".assignment-end-date").html(assignmentEndDate);
     $(this).find(".assignment-content").html(assignmentContent);
-    $(this).find(".assignment-edit").attr("data-assignment-id", assignmentId);
-    $(this).find(".assignment-edit").attr("data-assignment-class", classroomID);
-    $(this).find(".assignment-delete").attr("data-assignment-id", assignmentId);
-    $(this).find(".assignment-delete").attr("data-assignment-class", classroomID);
-  });
 
-  $(document).on("hide.bs.modal", "#new-assignment-modal", function (event) {
-    $("iframe").contents().find("body").empty();
-    $("form")[0].reset();
-  });
-
-  $(document).on("hide.bs.modal", "#edit-assignment-modal", function (event) {
-    $("#edit-assignment-modal").remove();
-  });
-
-  $(document).on("click", ".assignment-edit", function (event) {
-    var assignmentId = $(this).data("assignment-id");
-    var classroomID = $(this).data("assignment-class");
-    var link = classroomID + "/assignments/" + assignmentId + "/edit";
-    $.ajax({
-      type: "GET",
-      url: link
+    $(this).find(".assignment-edit").attr({
+      "data-assignment-id": assignmentId,
+      "data-assignment-class": classroomID,
+      "data-assignment-name": assignmentName,
+      "data-assignment-start-date": assignmentStartDate,
+      "data-assignment-end-date": assignmentEndDate,
+      "data-assignment-type": $link.data("type"),
+      "data-assignment-content": assignmentContent
     });
+
+
+    $(this).find(".assignment-delete").attr({
+      "data-assignment-id": assignmentId,
+      "data-assignment-class": classroomID
+    });
+  });
+
+  $(document).on("show.bs.modal", "#new-assignment-modal", function (event) {
+    $("form.new_assignment")[0].reset();
+    $("iframe").contents().find("body").empty();
+    $("#error_explanation").remove();
+    var $link = $(event.relatedTarget);
+
+    if ($link.data("assignment-id") != undefined) {
+      $("#show-assignment-modal").modal("hide");
+      var assignmentId = $link.attr("data-assignment-id");
+      var assignmentName = $link.attr("data-assignment-name");
+      var assignmentContent = $link.attr("data-assignment-content");
+      var assignmentStartDate = $link.attr("data-assignment-start-date");
+      var assignmentEndDate = $link.attr("data-assignment-end-date");
+      var assignmentType = $link.attr("data-assignment-type");
+      var classroomID = $link.attr("data-assignment-class");
+      var url = "/class_rooms/" + classroomID + "/assignments/" + assignmentId;
+
+      $form = $(this).find("form");
+      $form.attr("action", url);
+
+      $("<input>").attr({
+        type: "hidden",
+        value: "patch",
+        name: "_method"
+      }).appendTo($form);
+      $form.find("#assignment_name").val(assignmentName);
+      $form.find("#assignment_assignment_type").val(assignmentType);
+      $form.find("#assignment_start_date").val(assignmentStartDate.replace(/\//g, "-"));
+      $form.find("#assignment_end_date").val(assignmentEndDate.replace(/\//g, "-"));
+      CKEDITOR.instances.assignment_content.setData(assignmentContent);
+    }
   });
 
   $(document).on("click", ".assignment-delete", function (event) {
@@ -55,8 +81,4 @@ $(document).on("ready", function() {
     else
       return false;
   });
-
-  $(document).on("page:update", function () {
-    $(".ckeditor").ckeditor();
-  })
 });
