c1b9ec2e297e76fb26d3bec473e3bc291118a0da
TrungNT <hellovietnam93@gmail.com>
2016-04-21 18:54:06 +0700

    Update assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,59 +1,71 @@
 class AssignmentsController < ApplicationController
   load_and_authorize_resource
-  skip_load_resource only: :index
+  skip_load_resource only: [:index, :show]
+  before_action :find_classroom
 
   def index
     @class_room = ClassRoom.includes(:user_classes, assignments: [assignment_submits: :user]).
       find_by_id params[:class_room_id]
-    if @class_room
-      if check_lecturer_of_class? current_user, @class_room.user_classes
-        @assignment = @class_room.assignments.new if current_user.lecturer?
-      elsif is_member_of_class? current_user, @class_room.user_classes
-        @assignment_submit = @class_room.assignment_submits.new
-      else
-        redirect_to @class_room
-      end
-      @assignments = @class_room.assignments
+    if check_lecturer_of_class? current_user, @class_room.user_classes
+      @assignment = @class_room.assignments.new if current_user.lecturer?
+    elsif is_member_of_class? current_user, @class_room.user_classes
+      @assignment_submit = @class_room.assignment_submits.new
     else
-      flash[:dander] = t "flashs.messages.model_not_found",
-        model: "ClassRoom", id: params[:class_room_id]
+      redirect_to @class_room
     end
+    @assignments = @class_room.assignments
   end
 
   def create
-    respond_to do |format|
-      if @assignment.save
-        EventService.new(current_user.id, @assignment, params[:action]).save
-        @class_room = @assignment.class_room
-      end
-      format.js
+    if @assignment.save
+      EventService.new(current_user.id, @assignment, params[:action]).save
+      @class_room = @assignment.class_room
+      redirect_to [@class_room, @assignment]
+    else
+      render :new
     end
   end
 
   def show
+    @assignment = Assignment.includes(assignment_submits: :user).find params[:id]
+  end
+
+  def new
+
+  end
+
+  def edit
 
   end
 
   def update
-    respond_to do |format|
-      if @assignment.update_attributes assignment_params
-        EventService.new(current_user.id, @assignment, params[:action]).save
-        @class_room = @assignment.class_room
-        @assignments = @class_room.assignments
-      end
-      format.js
+    if @assignment.update_attributes assignment_params
+      EventService.new(current_user.id, @assignment, params[:action]).save
+      @class_room = @assignment.class_room
+      redirect_to [@class_room, @assignment]
+    else
+      render :edit
     end
   end
 
   def destroy
-    respond_to do |format|
-      @assignment.destroy
-      format.js
-    end
+    @assignment.destroy
+    redirect_to class_room_assignments_path @class_room
   end
 
   private
   def assignment_params
     params.require(:assignment).permit Assignment::ATTRIBUTES_PARAMS
+  end
+
+  def find_classroom
+    @class_room = ClassRoom.includes(:user_classes, assignments: [assignment_submits: :user]).
+      find_by_id params[:class_room_id]
+
+    unless @class_room
+      flash[:dander] = t "flashs.messages.model_not_found",
+        model: "ClassRoom", id: params[:class_room_id]
+      redirect_to class_rooms_path
+    end
   end
 end
