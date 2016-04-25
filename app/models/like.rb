54f983901b8e594392cbec8581a3be190a48f52d
TrungNT <hellovietnam93@gmail.com>
2016-04-25 21:44:54 +0700

    Update statistic

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,4 @@ class Like < ActiveRecord::Base
   belongs_to :user
 
   belongs_to :likeable, polymorphic: true
-
-  # scope :find_by_target, ->table_name, target_id {where target_id: target_id, target_type: table_name}
 end
