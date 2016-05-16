73672ecafae5d80d1b4e1a659c2badd2d790bd33
TrungNT <hellovietnam93@gmail.com>
2016-05-16 12:45:58 +0700

    update timetables

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -6,7 +6,7 @@ class TimetablesController < ApplicationController
       user_classes: :user).find params[:class_room_id]
     @timetable = Timetable.new
     @timetables = @class_room.timetables
-    @syllabus_details = @class_room.course.syllabus_details
+    load_syllabus_details
     @requests = @class_room.user_classes.select do |user_class|
       user_class.status == "waiting"
     end
@@ -18,15 +18,18 @@ class TimetablesController < ApplicationController
     redirect_to class_room_timetables_path @timetable.class_room
   end
 
-  def update
+  def edit
+    @class_room = @timetable.class_room
+    load_syllabus_details
     respond_to do |format|
-      if @timetable.update timetable_params
-        EventService.new(current_user.id, @timetable).save
-        format.json {render :index, location: @timetable.class_room }
-      else
-        format.json {render json: @timetable.errors, status: :unprocessable_entity }
-      end
+      format.js
     end
+  end
+
+  def update
+    @timetable.update timetable_params
+    EventService.new(current_user.id, @timetable).save
+    redirect_to class_room_timetables_path @timetable.class_room
   end
 
   def show
@@ -39,5 +42,12 @@ class TimetablesController < ApplicationController
   def timetable_params
     params.require(:timetable).permit :class_room_id, :title,
       :time_start, :time_end, :content, syllabus_detail_ids: []
+  end
+
+  def load_syllabus_details
+    @syllabus_details = []
+    @class_room.course.syllabuses.each do |syllabus|
+      @syllabus_details += syllabus.syllabus_details
+    end
   end
 end
